3bd4790f6e65e4f4b6cccb5709984ae1f8a19a68
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-25 20:04:11 +0600

    chore: Update PriceList component to fetch prices from JSON file

--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const PriceList = () => {
    const [prices, setPrices] =  useState([]);
+   useEffect (() => {
+         fetch('prices.json')
+         .then(res => res.json())
+         .then(data => setPrices(data))
+    
+   },[])
     return (
         <div>
             <h2 className='text-4xl bg-purple-300 text-purple-800 text-center p-2'>Awesome prices </h2>
+            
         </div>
     );
 };
