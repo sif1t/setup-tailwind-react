e3c2db2db8f0d6ea3c21d0939d83d7c125239d73
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-25 20:01:48 +0600

    feat: Add state hook for prices in PriceList component

--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PriceList = () => {
+   const [prices, setPrices] =  useState([]);
     return (
         <div>
             <h2 className='text-4xl bg-purple-300 text-purple-800 text-center p-2'>Awesome prices </h2>
