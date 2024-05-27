e0bb8e0deaab192d623ddcebaeb9be2608258099
Arifeen Sifat <arifeen.sifat01@gmail.com>
2024-05-27 18:03:11 +0600

    chore: Update PriceCard and PriceList components

--- src/components/PriceCard/PriceCard.jsx
+++ src/components/PriceCard/PriceCard.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 const PriceCard = ({price}) => {
     return (
-        <div>
-            {price.name}
+        <div className='font-semibold bg-gradient-to-t from-purple-300 to-gray-400 rounded-md p-4'>
+            <h2 className='text-center my-2'>
+                <span className='text-purple-800 text-2xl font-bold'>${price.price}</span>
+                <span className='text-purple-600 text-lg font-bold'>/month</span>
+            </h2>
+           <h4 className='text-2xl font-bold text-center'> {price.name}</h4>
         </div>
     );
 };
 
-export default PriceCard;
+export default PriceCard; 
--- src/components/PriceList/PriceList.jsx
+++ src/components/PriceList/PriceList.jsx
@@ -3,22 +3,24 @@ import React, { useEffect, useState } from 'react';
 import PriceCard from '../PriceCard/PriceCard';
 
 const PriceList = () => {
-   const [prices, setPrices] =  useState([]);
-   useEffect (() => {
-         fetch('/public/prices.json')
-         .then(res => res.json())
-         .then(data => setPrices(data))
-    
-   },[])
+    const [prices, setPrices] = useState([]);
+    useEffect(() => {
+        fetch('/public/prices.json')
+            .then(res => res.json())
+            .then(data => setPrices(data))
+
+    }, [])
     return (
         <div>
-            <h2 className='text-4xl bg-purple-300 text-purple-800 text-center p-2'>Awesome prices </h2>
+            <h2 className='text-4xl bg-purple-300 text-purple-800 text-center p-2 mb-3'>Awesome prices </h2>
+            <div className='grid grid-cols-3 gap-3'>
             {
-                prices.map (price => <PriceCard
-                    key={price.id} // Add the key prop here
-                    price={price}
-                />)
-            }
+                    prices.map(price => <PriceCard
+                        key={price.id} // Add the key prop here
+                        price={price}
+                    />)
+                }
+            </div>
         </div>
     );
 };
